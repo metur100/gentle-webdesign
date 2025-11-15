6093557a190870d77542b512a97ad70904b8fa94
Turkes, Medin <certidevelopment@gmail.com>
2025-11-15 22:17:02 +0100

    Try fix build and deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   images: {
     domains: [
       'upload.wikimedia.org',
